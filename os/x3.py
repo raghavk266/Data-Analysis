51f2268b54048dde7bf84a18315d8172e16221ad
jelybeansenjoyer <therealraghavkashyap26@gmail.com>
2022-12-02 20:40:26 +0530

    os done

--- os/x3.py
+++ os/x3.py
@@ -1,4 +1,9 @@
 import os
 file = open('./os/reader.txt',mode='r')
-file_contents = file.read()
-print(file_contents) 
+# file_contents = file.read()
+# print(file_contents) 
+with open('./os/reader.txt','r') as f:
+    # file_contents = f.read()
+    # print(file_contents)
+    f_lines = f.readlines()
+    print(f_lines[0].strip().split(','))
